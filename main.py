925d4b5419a4e9957c54361538330b592976b2ce
Teodor Vidakovic <teodorv22@gmail.com>
2024-10-30 09:04:42 +0100

    3.8

--- main.py
+++ main.py
@@ -5,16 +5,29 @@ import argparse
 from matplotlib import pyplot as plt
 import pandas as pd
 
+def calculate_mae(predicted_counts, true_counts):
+    errors = np.abs(np.array(predicted_counts) - np.array(true_counts))
+    print(true_counts)
+    print(predicted_counts)
+    print(errors)
+    mae = np.mean(errors)
+    return mae
+
+def read_true_counts(csv_path):
+    df = pd.read_csv(csv_path)
+    true_counts = {os.path.splitext(filename)[0]: count for filename, count in zip(df['picture'], df['toad_boo_bobomb'])}
+    return true_counts
+
 def load_image(path):
     return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
 
 def image_gray(image):
     return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
-def image_bin1(image_gs):
+def image_bin(image_gs):
     height, width = image_gs.shape[0:2]
     image_binary = np.ndarray((height, width), dtype=np.uint8)
-    ret, image_bin = cv2.threshold(image_gs, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    ret, image_bin = cv2.threshold(image_gs, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return image_bin
 
 def invert(image):
@@ -27,33 +40,50 @@ def display_image(image, color=False):
         plt.imshow(image, 'gray')
     plt.show()
 
-def image_clahe(image_hs):
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    return clahe.apply(image_hs)
-
-def label2rgb(image, markers):
-    cmap = plt.get_cmap('jet', len(np.unique(markers)))
-    return cmap(markers)
-
 def getToadAndBoo(img, hsv):
     lower_white = np.array([0,0,120])
     upper_white = np.array([180, 50, 255])
     mask_white = cv2.inRange(hsv, lower_white, upper_white)
 
-    #display_image(mask_white)
+    # display_image(mask_white)
 
-    contours, _ = cv2.findContours(mask_white, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours_barcode = []
+    # kernel = np.ones((3,3), np.uint8)
+    # opening = cv2.morphologyEx(mask_white, cv2.MORPH_OPEN, kernel, iterations=2)
+    # display_image(opening)
+    # closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel, iterations=1)
+    # display_image(closing)
+    # sure_bg = cv2.dilate(closing, kernel, iterations=3)
+    # display_image(sure_bg)
+
+    img_bin = image_bin(image_gray(img))
+    display_image(img_bin)
+    kernel = np.ones((3,3), np.uint8)
+    opening = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN, kernel, iterations=2)
+    display_image(opening)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel, iterations=2)
+    display_image(closing)
+    sure_bg = cv2.dilate(closing, kernel, iterations=2)
+    display_image(sure_bg)
+    dist_transform = cv2.distanceTransform(sure_bg, cv2.DIST_L2, 5)
+    display_image(dist_transform)
+    ret, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0) 
+    display_image(sure_fg)
+
+    sure_bg_8bit = cv2.convertScaleAbs(sure_bg)
+
+    contours, _ = cv2.findContours(sure_bg_8bit, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours_valid = []
 
     for contour in contours:
         center, size, angle = cv2.minAreaRect(contour)
         width, height = size
-        if width > 23 and width < 90 and height >35 and height < 100:
-            contours_barcode.append(contour)
+        if width > 10 and width < 100 and height >40 and height < 100:
+            contours_valid.append(contour)
     
-    print(len(contours_barcode))
-    cv2.drawContours(img, contours_barcode, -1, (255,0,0), 1)
+    print(len(contours_valid))
+    cv2.drawContours(img, contours_valid, -1, (255,0,0), 1)
     display_image(img)
+    return len(contours_valid)
     
 
 def getBlackBobomb(hsv):
@@ -88,47 +118,36 @@ def getBlueCaps(hsv):
     display_image(mask_blue)
 
 def removeBackground(img, hsv):
-    lower_green = np.array([30,40,40])
-    upper_green = np.array([90,255,255])
+    lower_green = np.array([11,0,30])
+    upper_green = np.array([115,255,255])
 
     mask = cv2.inRange(hsv, lower_green, upper_green)
     mask_inv = cv2.bitwise_not(mask)
-    mask_inv[620:, :] = 0
-    mask_inv[0:150, :] = 0
+    mask_inv[630:, :] = 0
+    mask_inv[0:170, :] = 0
+    mask_inv[570:630, 0:190] = 0
 
     result = cv2.bitwise_and(img, img, mask=mask_inv)
-
-    kernel = np.ones((3, 3), np.uint8)
-    result = cv2.erode(result, kernel)
+    result = cv2.GaussianBlur(result, (17, 17), 0)
     #display_image(result)
     return result
 
-def calculate_mae(predicted_counts, true_counts):
-    errors = np.abs(np.array(predicted_counts) - np.array(true_counts))
-    print(true_counts)
-    print(predicted_counts)
-    print(errors)
-    mae = np.mean(errors)
-    return mae
-
-def read_true_counts(csv_path):
-    df = pd.read_csv(csv_path)
-    true_counts = {os.path.splitext(filename)[0]: count for filename, count in zip(df['picture'], df['toad_boo_bobomb'])}
-    return true_counts
-
-
 
 def process_image(image_path):
+    count = 0
+
     img = load_image(image_path)
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     #display_image(hsv)
 
-    #getToadAndBoo(hsv)    
+    img_without_background = removeBackground(img, hsv)
+    hsv_without_background = cv2.cvtColor(img_without_background, cv2.COLOR_BGR2HSV)
+    # display_image(img_without_background)
+    count += getToadAndBoo(img_without_background, hsv_without_background)    
     #getBlackBobomb(hsv)
     #getRedBobomb(hsv)
     #getBlueCaps(hsv)
-    img_without_background = removeBackground(img, hsv)
-    hsv_without_background = cv2.cvtColor(img_without_background, cv2.COLOR_BGR2HSV)
+
     #display_image(hsv_without_background)
 
     #getToadAndBoo(img, hsv_without_background)
@@ -148,7 +167,7 @@ def process_image(image_path):
     # cv2.drawContours(img, contours_barcode, -1, (255,0,0), 1)
     #display_image(img)
 
-    return 1
+    return count
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process images from a dataset folder.')
@@ -163,7 +182,6 @@ if __name__ == "__main__":
 
     for filename in os.listdir(args.dataset_folder):
         if filename.endswith(".jpg") or filename.endswith(".png"):
-            print(filename)
             image_path = os.path.join(args.dataset_folder, filename)
             print(f"Processing {image_path}")
             detected_count = process_image(image_path)
